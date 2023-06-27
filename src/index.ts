f8a34d85f1e71ce40ecea86d6e67aa88104be83f
alanff97 <alan.figueroa.cl@gmail.com>
2023-06-27 19:42:11 +0200

    version 2 completed

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import { MatchResult } from './MatchResult';
 import { MatchReader } from './MatchReader';
+import { CsvFileReader } from './CsvFileReader';
 
-const reader = new MatchReader('football.csv');
-reader.read();
+//Create an object that satisfies the DataReader interface (for version 2)
+const csvFileReader = new CsvFileReader('football.csv');
+
+//Create an instance of MatchReader and pass in something satisfying the "DataReader" interface
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
+//matchReader.matches
 
 let manUnitedWins = 0;
 
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
